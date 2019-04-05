f1e4551465811180f076f0233c43fd870cce698b
KobiF <jakob.falus@greenwichschools.org>
2019-04-05 08:46:33 -0400

    Add files via upload

---
+++ Scripts/game/things.js
@@ -0,0 +1,105 @@
+
+class None{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.color="white";
+    this.type="none"
+  }
+}
+
+class Obsticle{
+  constructor(x,y,deadly,color){
+    this.x=x;
+    this.y=y;
+    this.deadly = deadly;
+    this.color = color;
+    this.type = "obsticle";
+  }
+}
+class Wall extends Obsticle{
+  constructor(x,y){
+    super(x,y,false,"lightblue");
+    this.name = "wall";
+  }
+}
+class Lava extends Obsticle{
+  constructor(x,y){
+    super(x,y,true,"red");
+    this.name = "lava";
+  }
+}
+
+class Player{
+  constructor(x,y){
+    this.x=x;
+    this.Xspeed = 0;
+    this.y=y;
+    this.Yspeed = 0;
+    this.color = "green"
+    this.type = "player";
+  }
+}
+
+class Door{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.color="brown";
+    this.type="door"
+  }
+}
+
+class Checkpoint{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.color="lime";
+    this.type="checkPoint"
+  }
+  getCoords(){
+    return [this.x,this.y];
+  }
+  setCoords(mat){
+    this.x = mat[0];
+    this.y = mat[1];
+  }
+}
+
+class keyDoor{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.color="gray";
+    this.type="keyDoor"
+  }
+}
+
+class key{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.color="yellow";
+    this.type="key"
+  }
+}
+
+class fakeWall{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+    this.color="lightblue";
+    this.type="fakeWall"
+  }
+}
+
+const allObjs = [];
+allObjs.push(new None(0,0));
+allObjs.push(new Player(0,0));
+allObjs.push(new Wall(0,0));
+allObjs.push(new Lava(0,0));
+allObjs.push(new Door(0,0));
+allObjs.push(new Checkpoint(0,0));
+allObjs.push(new keyDoor(0,0));
+allObjs.push(new key(0,0));
+allObjs.push(new fakeWall(0,0));
